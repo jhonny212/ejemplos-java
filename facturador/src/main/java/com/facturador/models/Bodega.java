ac8c04488fdd5c0e20233270f76b3e63d6db4405
Jhonny Garcia <47896718+jhonny212@users.noreply.github.com>
2025-02-05 23:05:35 -0600

    Implement methods

--- facturador/src/main/java/com/facturador/models/Bodega.java
+++ facturador/src/main/java/com/facturador/models/Bodega.java
@@ -4,10 +4,30 @@ import java.util.ArrayList;
 
 public class Bodega {
 
-    public ArrayList<Inventario> InventarioProducots;
+    //Azucar
+    //sal
+    //Leche
+    //cereal
+    public ArrayList<Inventario> InventarioProducots = new ArrayList<>();
 
     public int ValidarInventario(String codigoProducto, int cantidad) {
-        throw new IllegalStateException("");
+        //Dado un codigo validar la existencia en el listado de inventario products
+        //Buscar entre el listado el codigo product solicitado
+        for (int i = 0; i < this.InventarioProducots.size(); i++) {
+            Inventario item = this.InventarioProducots.get(i);
+
+            //Busco dado el codigo
+            if(item.getCodigo().equals(codigoProducto)){
+                //Si tenemos existencia suficiente a la cantidad retornar la cantidad
+                if(cantidad >= item.getStock()){
+                    return cantidad;
+                }else{
+                    return item.getStock();
+                }
+                //De lo contrario retornar lo disponible
+            }
+        }
+        return 0;
     }
 
     public void ActualizarInventario(ArrayList<Producto> productos){
--- facturador/src/main/java/com/facturador/models/Vendedor.java
+++ facturador/src/main/java/com/facturador/models/Vendedor.java
@@ -2,25 +2,53 @@ package com.facturador.models;
 
 import java.util.ArrayList;
 
-public class Vendedor extends Persona{
+public class Vendedor extends Persona {
 
     public Vendedor(String nombre, Long dpi) {
         super(nombre, dpi);
     }
-    
-    public boolean ValidarCliente(Long dpi){
-        throw new IllegalStateException("");
+
+    public boolean ValidarCliente(Long dpi) {
+        // Que dpi > 0
+        // Que entre solo numeros positivos
+        if (dpi > 0) {
+            // Que dpi tenga una longitud de 5 caracteres
+            String dpiTexto = dpi.toString();
+            if (dpiTexto.length() > 5) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
     }
 
-    public void PrepararFactura(ArrayList<Producto> productos, double dinero){
-        throw new IllegalStateException("");
+    public void PrepararFactura(ArrayList<Producto> productos, double dinero) {
+        //TODO
     }
 
-    public boolean ValidarDinero(ArrayList<Producto> productos, double dinero){
-        throw new IllegalStateException("");
+    public boolean ValidarDinero(ArrayList<Producto> productos, double dinero) {
+        // Validar si el "dinero" es suficiente a la suma de precio de los productos
+
+        // Obtener la suma de los precios
+        double sumaPrecios = 0;
+        for (int i = 0; i < productos.size(); i++) {
+            Producto producto = productos.get(i);
+            sumaPrecios = sumaPrecios + producto.getPrecio();
+        }
+
+        // Comparar la suma precios con el dinero del cliente
+        // Si dinero es mayor a suma precios 
+        if(dinero >= sumaPrecios){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public void EmitirFactura(){
-        throw new IllegalStateException("");
+    public void EmitirFactura() {
+        //TOD
     }
 }
